99cb18d38ed686a15680b5ed7b8e89c7684d1cb2
HARSHAN A M <146644928+harsshhan@users.noreply.github.com>
2024-03-24 05:58:17 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,51 @@
+function plus(){
+    var popupoverlay=document.querySelector('.popupoverlay')
+    popupoverlay.style.display="block"
+    var popupbox=document.querySelector('.popupbox')
+    popupbox.style.display="block"
+}
+var popupoverlay=document.querySelector('.popupoverlay')
+var popupbox=document.querySelector('.popupbox')
+function cancelbtn(event){
+    event.preventDefault()
+
+    popupoverlay.style.display="none"
+    popupbox.style.display="none"
+}
+
+var container=document.querySelector('.container')
+var addbook=document.getElementById('add-popup')
+
+
+
+addbook.addEventListener('click',function(event){
+    event.preventDefault()
+    var form=document.getElementById('form')
+    var bookname=document.getElementById('name').value
+    var author=document.getElementById('author').value
+    var description=document.getElementById('description').value
+    
+    if(bookname&author&description){
+            var div=document.createElement(div)
+            div.setAttribute('class','book')
+            div.innerHTML=`<h1>${bookname}</h1>
+            <h5>${author}</h5>
+            <p>${description}</p>
+            <button>Delete</button>`
+        
+            container.append(div)
+
+            popupoverlay.style.display="none"
+            popupbox.style.display="none" 
+            form.reset()
+    }
+    else{
+        alert("Fill all the field")
+    }
+
+
+})
+
+function deletebook(event){
+    event.target.parentElement.remove()
+}
